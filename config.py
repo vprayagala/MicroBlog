3c71c0b7dee1dcc11d46082f936b3a573daff1b7
Vijay Prayagala <vijay.prayagala@gmail.com>
2020-04-17 22:49:07 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,21 @@
+# Flask_WTF
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'TOP_SECRET'
+
+# Sqlite
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+# Pagination
+POSTS_PER_PAGE = 3
+
+# Mail
+MAIL_SERVER = os.environ.get('MAIL_SERVER')
+MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+ADMINS = ['your-email@example.com']
---
+++ run.py
@@ -0,0 +1,4 @@
+#! /opt/anaconda3/envs/de/bin/python3.6
+
+from app import app
+app.run(debug=True)
